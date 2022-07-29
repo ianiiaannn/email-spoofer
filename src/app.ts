ae471408e5f22e6301ebf0fec98dfbfb09df6168
ianiiaannn <ianiiaannn@gmail.com>
2022-07-29 11:57:04 +0800

    push

--- src/app.ts
+++ src/app.ts
@@ -1,8 +1,11 @@
 #! /usr/bin/env node
-import chalk from 'chalk';
 import { OptionValues, program } from 'commander';
 import dns, { MxRecord } from 'dns';
+import figlet from 'figlet';
+import gradient from 'gradient-string';
 import net from 'net';
+
+import { generateMessageID } from './message-id.js';
 
 const rainbow = {
   red: '\x1b[31m',
@@ -21,15 +24,18 @@ program
   .version(process.env.npm_package_version as string);
 
 program
-  .option(rainbow.red+'-h --help', rainbow.space+'Print help message.');
+  .option(rainbow.red + '-h --help', rainbow.space + 'Print help message.');
 program
-  .option(rainbow.green+'-f --from <string>', rainbow.space+'Email address to send from.');
+  .requiredOption(rainbow.green + '-f --from <string>', rainbow.space + 'Email address to send from.');
 program
-  .option(rainbow.yellow+'-t --to <string>', rainbow.space+'Email address to send to.');
-//program
-  //.option(rainbow.blue+'--cc \x5bstring]', rainbow.space+'cc email address.');
-//program
-//  .option('--bcc [string]', rainbow.blue('bcc email address.'));
+  .option(rainbow.yellow + '-t --to <string>', rainbow.space + 'Email address to send to.');
+
+program
+  .option(`-s --smtp [string]`, `SMTP server to use. (Default: from email domain)`);
+// program
+// .option(rainbow.blue+'--cc \x5bstring]', rainbow.space+'cc email address.');
+program
+  .option('--bcc [string]', 'bcc email address.');
 
 program
   .option('--subject [string]', 'Subject of the email.');
@@ -37,6 +43,11 @@ program
   .option('--body [string]', 'Body of the email.');
 program
   .option('-p --port [number]', 'Port to host SMTP server on.', '25');
+
+program
+  .option('--username', 'Username for SMTP server.');
+program
+  .option('--password', 'Password for SMTP server.');
 // .option('--attachment [string]', 'Attachment to send with the email.');
 console.log(rainbow.blue);
 program.parse();
@@ -51,8 +62,11 @@ if (!(option.from && option.to)) {
   process.exit(1);
 }
 
-// Note: option.to trigger some strange SMTP things.
-dns.resolveMx(option.from.split('@')[1], (err, address: MxRecord[]) => {
+if (!option.smtp) {
+  option.smtp = option.to.split('@')[1];
+}
+
+dns.resolveMx(option.smtp, (err, address: MxRecord[]) => {
   if (err) {
     console.log('Please check your internet connection.');
     process.exit(1);
@@ -65,30 +79,42 @@ dns.resolveMx(option.from.split('@')[1], (err, address: MxRecord[]) => {
       console.log(`Connected to ${server} on port ${option.port}`);
     });
   const CRLF: string = '\r\n';
-  const payload: string[] = [
-    `HELO ${server}`,
-    // `STARTSSL`,
-    `MAIL FROM: <${option.from}>`,
-    `RCPT TO: <${option.to}>`,
-    `DATA`,
-    `Subject: ${option.subject || 'No Subject'}
-From: <${option.from}>
-To: <${option.to}>
-Cc: <${option.cc || ''}>
-Bcc: <${option.bcc || ''}>
-${option.body || ''}
-${CRLF}.`,
-    /* 'DATA\r\n.\r\n',*/
-    `QUIT`,
-  ];
+  const payload: string[] = [`HELO ${server}`];
+  if (option.username && option.password) {
+    payload.push(`STARTTLS`);
+    payload.push(`AUTH`);
+    payload.push(`${option.username.toString('base64')}`);
+    payload.push(`${option.password.toString('base64')}`);
+  }
+  payload.push(`MAIL FROM: <${option.from}>`);
+  payload.push(`RCPT TO: <${option.to}>`);
+  payload.push(`DATA
+From: ${option.from}
+To: ${option.to}`);
+  if (option.cc) {
+    payload[payload.length - 1] += (`Cc: ${option.cc}`);
+  }
+  if (option.bcc) {
+    payload[payload.length - 1] += (`Bcc: ${option.bcc}`);
+  }
+  payload[payload.length - 1] += (`
+Message-ID: <${ generateMessageID(option.from.split('@')[1])}>
+Subject: ${ option.subject }
+
+${ option.body }
+
+${ CRLF }.`);
+  payload.push(`QUIT`);
   let index: number = 0;
   socket.on('data', (data: Buffer) => {
     const response: string = data.toString();
     console.log(response);
     if (index <= payload.length) {
-      socket.write(`${payload[index]}${CRLF}`);
-      console.log(`> ${payload[index]}`);
+      socket.write(`${ payload[index] }${ CRLF } `);
+      console.log(`> ${ payload[index] } `);
       index++;
+    } else {
+      socket.end();
     }
   });
 });
---
+++ src/message-id.ts
@@ -0,0 +1,25 @@
+import os from 'os';
+
+// From https://github.com/jordan-wright/email/blob/master/email.go
+
+// generateMessageID generates and returns a string suitable for an RFC 2822
+// compliant Message-ID, e.g.:
+// <1444789264909237300.3464.1819418242800517193@DESKTOP01>
+//
+// The following parameters are used to generate a Message-ID:
+// - The nanoseconds since Epoch
+// - The calling PID
+// - A cryptographically random int64
+// - The sending hostname
+/**
+ * Generate a message ID
+ * 
+ * @return {string} A valid messageID
+ */
+export function generateMessageID(hostname:string): String {
+  const time: Number = Math.floor(new Date().getTime() / 1000);
+  const pid: number = process.pid;
+  const randomInt: Number = Math.floor(Math.random() * 2 ^ 53);
+  const messageID: string = (`${time}.${pid}.${randomInt}@${hostname}`);
+  return messageID;
+}
